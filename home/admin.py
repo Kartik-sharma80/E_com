e0a71c09c1077ce4aa1337c89c35967767816c99
Kartik-sharma80 <49423167+Kartik-sharma80@users.noreply.github.com>
2020-05-17 21:04:24 +0530

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from home.models import Contact
+from home.models import User
 
 # Register your models here.
-admin.site.register(Contact)
+admin.site.register(User)
+#admin.site.register(Contact)
