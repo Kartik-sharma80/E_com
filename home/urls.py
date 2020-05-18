2bcd8d1d5b96dcfd8c272286d411f1fccc1d5cd6
Kartik-sharma80 <49423167+Kartik-sharma80@users.noreply.github.com>
2020-05-19 01:07:48 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -2,15 +2,23 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
+
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path("signup",views.Signup.as_view(),name="signup"),
-    path('aftersign/',views.Aftersign.as_view()),
+	path('',views.index,name='index'),
+	path('register/', views.signup,name='signup'),
+	path('login/', views.Login,name='Login'),
+	
+]
+	
+    #path('', views.home, name='home'),
+   # path("signup",views.Signup.as_view(),name="signup"),
+    #path('aftersign/',views.Aftersign.as_view()),
    # path('shop', views.shop, name='shop'),
     #path('contact',views.contact, name='contact'),
     #path('login',views.login,name='login'),
-    #path('test',views.test,name='test')
-]
+    #path('test',views.test,name='test')'''
+
 ''' path('about',views.about, name='about'),
     path('service',views.service, name='service'),
     path('service/IceCreams',views.service, name='service'),
